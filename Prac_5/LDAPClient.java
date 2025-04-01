804666cc8d61928b00096939c85a18b501f18fb0
Zainab <abdulrasaqzainab0@gmail.com>
2025-04-01 12:21:16 +0200

    Update LDAPClient.java

--- Prac_5/LDAPClient.java
+++ Prac_5/LDAPClient.java
@@ -15,9 +15,36 @@ public class LDAPClient {
         try{
             Socket socket = new Socket(LDAP_HOST, LDAP_PORT);
             System.out.println("Connected to LDAP server at " + LDAP_HOST + ":" + LDAP_PORT);
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+
 
             // Send a simple LDAP bind request
-            String bindRequest =
+            byte[] bindRequest = constructBindRequest("cn=admin,dc=example,dc=com", "password");
+            outputStream.write(bindRequest);
+            outputStream.flush();
+            System.out.println("Sent bind request to LDAP server.");
+
+            // Read the response from the server
+            byte[] response = new byte[1024];
+            int bytesRead = inputStream.read(response);
+            System.out.println("Bind Response: " + Arrays.toString(Arrays.copyOf(response, bytesRead)));
+
+            //Construct Search Request
+            byte[] searchRequest = constructSearchRequest("dc=example,dc=com", "(objectClass=*)");
+            outputStream.write(searchRequest);
+            outputStream.flush();
+            System.out.println("Sent search request to LDAP server.");
+
+            // Read the response from the server
+            bytesRead = inputStream.read(response);
+            System.out.println("Search Response: " + Arrays.toString(Arrays.copyOf(response, bytesRead)));
+
+            // Close the streams and socket
+            socket.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
