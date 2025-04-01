68760a48ebdc6b54cbf03e35c0022326d73df543
Zainab <abdulrasaqzainab0@gmail.com>
2025-04-01 14:32:15 +0200

    Update LDAPClient.java

--- Prac_5/LDAPClient.java
+++ Prac_5/LDAPClient.java
@@ -48,5 +48,28 @@ public class LDAPClient {
         }
     }
 
-    
-}
+    private static byte[] constructBindRequest(String din, String password)
+    {
+        byte[] dnBytes = dn.getBytes();
+        byte[] passwordBytes = password.getBytes();
+
+        return new byte[]{
+            0x30, (byte) (dnBytes.length + passwordBytes.length + 11), // LDAPMessage (SEQ)
+            0x02, 0x01, 0x01, // Message ID: 1 (INTEGER)
+            0x60, (byte) (dnBytes.length + passwordBytes.length + 7), // BindRequest (Application[0])
+            0x02, 0x01, 0x03, // Version (INTEGER: 3)
+            0x04, (byte) dnBytes.length // DN (OCTET STRING)
+        };
+    }
+
+    // Manually constructs an LDAP Search Request
+    private static byte[] constructSearchRequest(String baseDN) {
+        byte[] baseDNBytes = baseDN.getBytes();
+        return new byte[]{
+            0x30, (byte) (baseDNBytes.length + 12),
+            0x02, 0x01, 0x02, // Message ID: 2
+            0x63, (byte) (baseDNBytes.length + 8), // SearchRequest (Application[3])
+            0x04, (byte) baseDNBytes.length // Base DN
+        };
+
+    }        
